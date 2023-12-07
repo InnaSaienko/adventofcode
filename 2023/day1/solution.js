3b597e718f3ff3eb56566abb36e7bcad88966c28
Inna Saienko <inna.saienko.nl@gmail.com>
2023-12-07 20:54:41 +0100

    2023: added solution for day 1 part 2

--- 2023/day1/solution.js
+++ 2023/day1/solution.js
@@ -11,34 +11,58 @@ function readData() {
     return data;
 }
 
+const strToNum = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+};
+
+const regexBeg = /.*?(one|two|three|four|five|six|seven|eight|nine|\d)/;
+const regexEnd = /.*(one|two|three|four|five|six|seven|eight|nine|\d).*$/;
+
+function getNum(str) {
+    if (strToNum.hasOwnProperty(str)) {
+        return strToNum[str];
+    } 
+    return str;
+}
+
 function extractNumber(inStr) {
-    const regex = /^(?:.*?(\d).*(\d).*|.*(\d).*)$/m;
-    let numArray = inStr.match(regex);
-    if (numArray == null) {
-        return null;
-    }
-    let cleanArr = numArray.slice(1).filter(Number);
+    let num1arr = inStr.match(regexBeg);
+    let num2arr = inStr.match(regexEnd);
+    let numArray = [];
+    numArray.push(num1arr[1], num2arr[1]);
+    let numbersArr = numArray.map(getNum);
+    let cleanArr = numbersArr.filter(Number);
     if (cleanArr.length == 1) {
-        cleanArr.push(cleanArr[0]);
+         cleanArr.push(cleanArr[0]);
     }
-    let numStr = cleanArr.join('');
+    let numStr = numbersArr.join('');
     return Number(numStr);
 }
 
 function main() {
     const data = readData();
+    
     if (data == null) {
         console.error("Data is null");
         return;
     } 
+
     const dataArr = data.split("\r\n");
+
     let sum = 0;
+    
     for(let i = 0; i < dataArr.length; i++) {
-        let number = extractNumber(dataArr[i]);
-        if (number !== null) {
-            sum += number;
-        }
+        sum += extractNumber(dataArr[i]);
     }
+
     return sum;
 }
 
